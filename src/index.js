c2660b13ed29cd51cd36fed125cb90f46a6fc85e
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-04-28 13:38:59 +0100

    Plug in basics posts list

--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+import PostsIndex from './components/posts_index'
 
-class Hello extends React.Component {
-    render() {
-        return <div>Hello!</div>
-    }
-}
+import reducers from './reducers'
 
-class Goodbye extends React.Component {
-    render() {
-        return <div>Goodbye!</div>
-    }
-}
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <Route path="/hello" component={Hello} />
-      <Route path="/goodbye" component={Goodbye} />
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostsIndex} />
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('.container')
+)
