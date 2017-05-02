6e24b834e12332f43e65b1a3770ad94f01c9ddc2
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-02 20:17:00 +0100

    Render list of posts to screen

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
-    componentDidMount() {
-        this.props.fetchPosts()
-    }
+	componentDidMount() {
+		this.props.fetchPosts()
+	}
 
-    render() {
-        return (
-            <div>PostsIndex</div>
-        )
-    }
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li key={post.id} className="list-group-item">
+					{post.title}
+				</li>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
+			</div>
+		)
+	}
 }
 
-export default connect(null, { fetchPosts })(PostsIndex)
+function mapStateToProps(state) {
+	return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
