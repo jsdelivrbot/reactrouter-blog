44d0d6686660a981fc756cbe225b4f4fbdb3ff8a
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-04 22:48:30 +0100

    connect fetch_post into redux and extract needed post to this.props.post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params
+        this.props.fetchPost(id)
+    }
+
     render() {
         return (
             <div>Posts Show</div>
@@ -8,4 +15,8 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow)
